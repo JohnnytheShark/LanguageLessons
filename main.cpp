ca423c974ed330722f1d3d148342ed019f1b3e36
JohnnytheShark <orellanajonathan7@gmail.com>
2020-11-22 20:06:12 -0500

    Added C++ Reference Sheet

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// This is how you handle a single line comment
+
+/* Multiline notes are added in using the following format
+This file is used to describe all the various different methods available within c++ 
+This serves as documentation for future projects and as reference
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    return 0;
+}
